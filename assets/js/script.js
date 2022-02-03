bd92dc8d92ac33fa7b2c23397cede75aa57c4d6b
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-02-02 18:44:21 -0800

    added eventlistener for zipcode input, zipcode stores in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@
 var $modal = $(".modal");
 // Coords to center the map initially
 var coords = { lat: 47.6142, lng: -122.1937 };
+// creating input variable and search button variable
+var zipInput = document.querySelector("#zip");
+var searchBtn = document.querySelector("#button1");
+// will be used later to pull zip value
+var getZip;
 
 // modal
 $modal.dialog({
@@ -55,13 +60,13 @@ var initMap = function () {
       infowindow.setContent(content);
       infowindow.open(map, marker);
     });
-  }
+  };
 
   var request = {
     location: coords,
     radius: '500', // Preferring results closer to the center point.
     query: 'community garden',
-  }
+  };
 
   service = new google.maps.places.PlacesService(map);
 
@@ -73,4 +78,17 @@ var initMap = function () {
       }
     }
   });
-}
+};
+
+
+
+
+
+
+searchBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  getZip = zipInput.value.trim();
+  console.log(getZip);
+  // this is optional, if we don't want to store zipcodes we can scratch this
+  localStorage.setItem("zip", JSON.stringify(getZip));
+});
