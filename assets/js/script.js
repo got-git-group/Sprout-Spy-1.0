dd035e84c233f3d562f4cb0568cfcd991166d972
Andrew An <andyan7@gmail.com>
2022-02-02 19:01:27 -0800

    styled glossary, sub-glossary, and results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // creating modal variable
 var $modal = $(".modal");
+
 // Coords to center the map initially
 var coords = { lat: 47.6142, lng: -122.1937 };
 
@@ -24,7 +25,6 @@ $modal.dialog({
 });
 
 // Google maps
-
 var initMap = function () {
 
   map = new google.maps.Map(document.getElementById('localgardenmap'), {
